0449429ba52309d7a5b66191952a2516e2f4a8ee
Kvphalien <kaphgrey@gmail.com>
2023-11-28 03:52:43 -0800

    updated

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
-for tens in range(10):
-    for ones in range(tens + 1, 10):
-        print("{:d}{:d}".format(tens, ones), end=(", " if tens != 8 or ones != 9 else "\n"))
 
+"""Print all possible different combinations of two digits in ascending order.
+
+    The two digits must be different - 01 and 10 are considered identical.
+    """
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, digit2), end=", ")
