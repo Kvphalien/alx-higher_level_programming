62302c8dd18add84ccc8afb4ec4caa27e05b0d1a
Kvphalien <kaphgrey@gmail.com>
2023-12-18 11:33:31 -0800

    updated

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 def raise_exception_msg(message=""):
-  raise NameError(message)
-
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
 
